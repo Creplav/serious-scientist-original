be6d72f0843f48504531770eed6880fdf044c487
Creplav <creplav@gmail.com>
2017-05-08 14:29:16 -0400

    Created abstract Cure class for use as a base class for all cures

---
+++ core/src/com/anywareinteractive/seriousscientist/cures/Cure.java
@@ -0,0 +1,11 @@
+package com.anywareinteractive.seriousscientist.cures;
+
+import java.util.Timer;
+import java.util.TimerTask;
+
+/**
+ * Created by Ben on 5/8/2017.
+ */
+public abstract class Cure {
+
+}
