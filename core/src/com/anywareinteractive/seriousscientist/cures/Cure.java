eaf6982bf2d1835cbe828793b7d61845d38dac75
Creplav <creplav@gmail.com>
2017-05-08 14:35:27 -0400

    Added protected variables to Cure.java class for use by other cures

--- core/src/com/anywareinteractive/seriousscientist/cures/Cure.java
+++ core/src/com/anywareinteractive/seriousscientist/cures/Cure.java
@@ -7,5 +7,10 @@ import java.util.TimerTask;
  * Created by Ben on 5/8/2017.
  */
 public abstract class Cure {
-
+    protected int cost = 0;
+    protected int price = 0;
+    protected int seriousnessInfection = 0;
+    protected int researchCost = 0;
+    protected int researchTime = 0;
+    protected String description = "No Description";
 }
