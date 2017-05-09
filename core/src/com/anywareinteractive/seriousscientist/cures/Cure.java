d5b2a3493841783159a9974ae363979484876fc3
Creplav <creplav@gmail.com>
2017-05-09 12:02:49 -0400

    Revert "Added protected variables to Cure.java class for use by other cures"
    This reverts commit eaf6982bf2d1835cbe828793b7d61845d38dac75.

--- core/src/com/anywareinteractive/seriousscientist/cures/Cure.java
+++ core/src/com/anywareinteractive/seriousscientist/cures/Cure.java
@@ -4,10 +4,5 @@ package com.anywareinteractive.seriousscientist.cures;
  * Created by Ben on 5/8/2017.
  */
 public abstract class Cure {
-    protected int cost = 0;
-    protected int price = 0;
-    protected int seriousnessInfection = 0;
-    protected int researchCost = 0;
-    protected int researchTime = 0;
-    protected String description = "No Description";
+
 }
