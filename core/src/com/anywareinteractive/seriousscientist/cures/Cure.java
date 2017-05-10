db065e9b9c597cfada3d545a78f1cf013402c59e
Creplav <creplav@gmail.com>
2017-05-10 16:21:44 -0400

    Added IBreakable interface in cures package for breaking cures

--- core/src/com/anywareinteractive/seriousscientist/cures/Cure.java
+++ core/src/com/anywareinteractive/seriousscientist/cures/Cure.java
@@ -3,7 +3,7 @@ package com.anywareinteractive.seriousscientist.cures;
 /**
  * Created by Ben on 5/10/17.
  */
-public abstract class Cure {
+public abstract class Cure implements IBreakable{
     protected int cost = 0;
     protected int price = 0;
     protected int seriousnessInfection = 0;
@@ -15,5 +15,16 @@ public abstract class Cure {
     public Cure createCure(Cure cure){
         //Set up a timer for cure creation.
         return cure;
+    }
+
+    @Override
+    public void drop() {
+        System.out.println(String.format("{} has dropped their cure. How clumsy! This will have no effect on serious or sick populations.", "Bob"));
+        breakItem();
+    }
+
+    @Override
+    public void breakItem() {
+
     }
 }
---
+++ core/src/com/anywareinteractive/seriousscientist/cures/IBreakable.java
@@ -0,0 +1,9 @@
+package com.anywareinteractive.seriousscientist.cures;
+
+/**
+ * Created by ben on 5/10/17.
+ */
+public interface IBreakable {
+    void drop();
+    void breakItem();
+}
