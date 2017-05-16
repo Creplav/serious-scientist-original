459520fe4fc37b7adc588a4bf2449b84781fa27a
Creplav <creplav@gmail.com>
2017-05-15 22:41:19 -0400

    Added threading for training, still WIP. Created a test class.

--- core/src/com/anywareinteractive/seriousscientist/controller/GameController.java
+++ core/src/com/anywareinteractive/seriousscientist/controller/GameController.java
@@ -2,9 +2,9 @@ package com.anywareinteractive.seriousscientist.controller;
 
 import com.anywareinteractive.seriousscientist.people.Scientist;
 
+import java.util.ArrayList;
+
 /**
- * Created by ben on 5/13/17.
- *
  * This class is for a good portion of the game logic. It will hold all of the important stuff like money and population.
  */
 public class GameController {
@@ -34,23 +34,14 @@ public class GameController {
      */
     public float money = 1000.0f;
 
-    //=========
-    // Current employees
-    //=========
-
-    // There is no need to go above 2.1 billion scientists and cashiers. Plus that would be insanely expensive. Let's use an integer.
-    public int currentScientists = 1;
-    public int currentCahsiers = 1; // Max 4 per store
-
     //=======
     // Scientists
     //=======
     public float scientistCost = 10000f;
     public float scientistPopulationRequirement = 20000f;
-    //TODO Create a training time for scientists.
+    public int scientistTrainingTime = 5; // In minutes
 
-    //TODO Create a collection for scientists
-
+    public ArrayList<Scientist> scientists = new ArrayList<Scientist>();
 
 
 
@@ -60,6 +51,7 @@ public class GameController {
 
     public GameController(String scientistName){
         Scientist scientist = new Scientist(scientistName);
+        scientists.add(scientist);
     }
 
     //**********************
@@ -70,28 +62,85 @@ public class GameController {
      * This method purchases a new scientist
      * @return
      */
-    public Scientist purchaseScientist(){
+    public void purchaseScientist(){
         // Make sure the player has met the requirements
-        if(money > scientistCost){
+        if(money < scientistCost){
             System.out.println("You do not have enough money to purchase the scientist.");
             System.out.println(String.format("You need %s more money", scientistCost - money));
-            return null;
         }
         else if(seriousPopulation < scientistPopulationRequirement){
             System.out.println("You do not have the required serious population to purchase the scientist.");
             System.out.println(String.format("You need %s more people", scientistPopulationRequirement - seriousPopulation));
-            return null;
         }
         // They've got it! Let them purchase one.
         else {
             money -= scientistCost;
             //TODO Create a new thread here and make it wait until the time is complete for training.
+            TrainingTimer trainingTimer = new TrainingTimer("Training Timer 1", this.scientistTrainingTime);
+            trainingTimer.run();
+            try {
+                while(trainingTimer.isAlive()){
+                    System.out.println("Waiting for thread to finish...");
+                    Thread.sleep(1000);
+                }
+            }
+            catch(InterruptedException e){
+                System.out.println("Main thread was interrupted.");
+            }
+
             Scientist scientist = new Scientist("Bob");
-            currentScientists++;
+            scientists.add(scientist);
             // Absolutely no reason for these numbers. Just picked them.
             scientistCost *= 3.14;
             scientistPopulationRequirement *= 2.3;
-            return scientist;
+            scientistTrainingTime = (scientists.size() * scientistTrainingTime * 2);
         }
     }
 }
+
+/**
+ * This is a class for creating a training timer for new scientists.
+ */
+class TrainingTimer extends Thread {
+
+    int executionTime;
+    String threadName;
+
+    TrainingTimer(String name, int minutes) {
+        this.executionTime = minutes;
+        this.threadName = name;
+        System.out.println("Created a new thread " + this.threadName);
+    }
+
+    public void run() {
+        try {
+            for (int i = this.executionTime - 1; i > -1; i--) {
+                for(int j = 60; j > 0; j--){
+                    Thread.sleep(1000);
+                    System.out.println((i) + ":" + j);
+                }
+                System.out.println("Time remaining: " + (i));
+            }
+        }
+        catch(InterruptedException e){
+            System.out.println("Warning! " + this.threadName + " was interrupted!");
+        }
+
+        System.out.println(this.threadName + " has stopped.");
+    }
+}
+
+/**
+ * To test functionality of the game thus far.
+ */
+class Test {
+    public static void main(String[] args) {
+        GameController gameController = new GameController("Tim");
+        System.out.println("Current scientists: " + gameController.scientists.size());
+        gameController.purchaseScientist();
+        gameController.money += 1000000.00f;
+        gameController.seriousPopulation += 10000000.00f;
+        gameController.purchaseScientist();
+    }
+}
+
--- core/src/com/anywareinteractive/seriousscientist/people/Scientist.java
+++ core/src/com/anywareinteractive/seriousscientist/people/Scientist.java
@@ -1,11 +1,10 @@
 package com.anywareinteractive.seriousscientist.people;
 
 /**
- * Created by ben on 5/10/17.
  * This class is for creating new scientists and upgrading them.
  */
 public class Scientist extends Person {
-    float efficency = 0.2f;
+    float efficiency = 0.2f;
 
     public Scientist(String name) {
         this.name = name;
@@ -13,14 +12,18 @@ public class Scientist extends Person {
         this.patience = 100;
     }
 
-    public float upgradeEfficency(){
-        if(this.efficency != 1.0f){
-            this.efficency += 0.1f;
-            return this.efficency;
+    /**
+     * This method upgrades the efficiency of the scientist in order to purduce more cures.
+     * @return
+     */
+    public float upgradeEfficiency(){
+        if(this.efficiency != 1.0f){
+            this.efficiency += 0.1f;
+            return this.efficiency;
         }
         else {
             System.out.println("Scientist is at max efficiency");
-            return this.efficency;
+            return this.efficiency;
         }
     }
 }
